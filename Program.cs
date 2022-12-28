789f81f37a754f7e02705317f55a321b9896f2c9
[rauf] <[raufam@code.edu.az]>
2022-12-28 18:48:36 +0400

    init

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace hw4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, hasil, i, count, a, b, c, d;
+            Console.WriteLine("eded daxil edin:");
+            num = int.Parse(Console.ReadLine());
+            a = num / 1000;
+            b = (num / 100) % 10;
+            c = (num / 10) % 10;
+            d = num % 10;
+            hasil = 1 * a + 2 * b + 3 * c + 4 * d;
+            Console.WriteLine(hasil);
+
+        }
+    }
+}
